b19e5273620e54b1389703015efd3329e16ee9d2
ibrahim uylas <ibrahim@uylas.net>
2021-05-06 15:03:12 +0100

    installed react-router and eslint

---
+++ src/views/HomeView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomeView extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Home Page
+            </div>
+        );
+    }
+}
+
+export default HomeView;
